19b62e504dbd6d3e655368b90ec6e190f2fb46c0
chengxinsong <chengxinsong@bytedance.com>
2020-06-26 17:34:48 +0800

    feat: 修改配置

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const db = {
     port: 3306, // 数据库端口
     database: 'koa-template', // 数据库名称
     user: 'xxx', // 数据库用户名
-    password: 'xxxxx', // 数据库密码,
+    password: 'xxxxxxx', // 数据库密码,
     salt: 'koa-template-sec',
 }
 
